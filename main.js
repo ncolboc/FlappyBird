3d18e984ba9f6dd74a035322022c7f578b30a526
MacFly! <ncolboc@gmail.com>
2015-04-14 00:46:53 +0200

    - create menu - update presentation

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ gameState.load.prototype = {
 
         //load sprites
         this.game.load.image('background','assets/img/background.png');
+        this.game.load.image('title','assets/img/flappy.png');
+        this.game.load.image('title_get_ready','assets/img/ready.png');
+        this.game.load.image('ground','assets/img/ground.png');
         this.game.load.atlasJSONHash('bird','assets/img/bird.png','assets/data/bird.json');
     },
     create: function () {
@@ -34,6 +37,20 @@ gameState.main.prototype = {
         this.background.width = this.game.width;
         this.background.height = this.game.height;
 
+        this.title = this.game.add.sprite(0, 0, 'title');
+        this.title.x = (this.game.width - this.title.width)/2;
+        this.title.y = 50;
+
+        this.title_ready = this.game.add.sprite(0, 0, 'title_get_ready');
+        this.title_ready.x = (this.game.width - this.title_ready.width)/2;
+        this.title_ready.y = 250;
+
+        this.ground = this.game.add.sprite(0, 0, 'ground');
+        this.ground.x = 0;
+        this.ground.y = this.game.height-this.ground.height;
+        this.ground.body.velocity.x = -250;
+        this.ground.body.immovable = true;
+
         this.bird = this.game.add.sprite(200,0,'bird');
         this.bird.width = this.bird.width / 6.5;
         this.bird.height = this.bird.height / 6.5;
@@ -46,7 +63,9 @@ gameState.main.prototype = {
         this.tweenFlap.to({ y: this.bird.y + 20}, 400, Phaser.Easing.Quadratic.InOut, true, 0, 10000000000, true);
     },
     update:function(){
-
+        if(this.ground.x + this.ground.width / 2 <= 0) {
+            this.ground.x = 0;
+        }
     }
 };
 
